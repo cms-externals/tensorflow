8b0616e2a42e2487437bee4873239b35469d74b3
Ivan Razumov <iarspider@gmail.com>
2024-09-18 16:42:09 +0200

    Remove one more reference to deprecated distutils module

--- tensorflow/lite/python/convert.py
+++ tensorflow/lite/python/convert.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 """Converts a frozen graph into a TFLite FlatBuffer."""
 
-import distutils.spawn
 import enum
 import hashlib
 import os as _os
@@ -23,6 +22,7 @@ import subprocess as _subprocess
 import tempfile as _tempfile
 from typing import Optional
 import warnings
+import shutils
 
 from tensorflow.compiler.mlir.quantization.stablehlo import quantization_config_pb2
 from tensorflow.compiler.mlir.quantization.stablehlo import quantization_options_pb2 as quant_opts_pb2
@@ -399,7 +399,7 @@ def _run_deprecated_conversion_binary(
     RuntimeError: When conversion fails, an exception is raised with the error
       message embedded.
   """
-  if distutils.spawn.find_executable(_deprecated_conversion_binary) is None:
+  if shutils.which(_deprecated_conversion_binary) is None:
     raise ConverterError("""Could not find `toco_from_protos` binary, make sure
 your virtualenv bin directory or pip local bin directory is in your path.
 In particular, if you have installed TensorFlow with --user, make sure you
